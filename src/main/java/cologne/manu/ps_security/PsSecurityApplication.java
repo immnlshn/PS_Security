fce6d23371d53e6a1f77d7a979135ff1adfbb9fc
Immanuel Sohn <info@immanuelsohn.de>
2023-12-08 21:24:18 +0100

    Initial commit

---
+++ src/main/java/cologne/manu/ps_security/PsSecurityApplication.java
@@ -0,0 +1,13 @@
+package cologne.manu.ps_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cologne/manu/ps_security/PsSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package cologne.manu.ps_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
