d7ee0d0141b7d73b1a07775660c884ff08933a78
Immanuel Sohn <info@immanuelsohn.de>
2023-12-14 10:11:29 +0100

    added github auth as a test

--- src/main/java/cologne/manu/ps_security/Controller/SampleControllerImpl.java
+++ src/main/java/cologne/manu/ps_security/Controller/SampleControllerImpl.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -12,6 +13,7 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/api/sample")
 public class SampleControllerImpl implements SampleController {
 
     @GetMapping(
